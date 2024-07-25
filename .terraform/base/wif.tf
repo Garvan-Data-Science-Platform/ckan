f210d94410a5f441b68b02c23c07f651efbbb52d
Tim Kallady <t.kallady@garvan.org.au>
2024-07-25 16:08:04 +1000

    cicd

--- .terraform/base/wif.tf
+++ .terraform/base/wif.tf
@@ -2,7 +2,17 @@
 # Service account associated with workload identity pool
 
 
+resource "google_project_service" "wif_api" {
+  for_each = toset([
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "sts.googleapis.com",
+  ])
 
+  service            = each.value
+  disable_on_destroy = false
+}
 
 module "gh_oidc" {
   source            = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
