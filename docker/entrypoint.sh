eb0e3815f1691f87ff356f12615e192c6ae17612
Tim Kallady <t.kallady@garvan.org.au>
2024-08-08 18:18:55 +1000

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,5 @@
 
 ./docker/parse-config.sh docker/ckan.ini.template > /app/ckan.ini && \
     ckan -c /app/ckan.ini db init && \ 
+    ckan -c /app/ckan.ini db upgrade -p pages && \
     gunicorn --bind 0.0.0.0:8000 wsgi:application
---
+++ pages/user_guide.md
@@ -0,0 +1,4 @@
+# User Guide
+
+This is a user guide
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](https://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID_HERE)
