9955cd32aee98d7ed92253cd557572b4aea21e90
tkallady <43975406+tkallady@users.noreply.github.com>
2024-08-29 12:25:30 +1000

    disable-email-login (#13)
    * Started feature disable-email-login
    
    * Disable anon access. Disabled email access and invite
    
    * Fix lint

---
+++ noanonaccess/README.md
@@ -0,0 +1,65 @@
+[![Tests](https://github.com/datopian/ckanext-noanonaccess/actions/workflows/test.yml/badge.svg)](https://github.com/datopian/ckanext-noanonaccess/actions/workflows/test.yml) [![CKAN](https://img.shields.io/badge/ckan-2.10.x-blue.svg)]() [![CKAN](https://img.shields.io/badge/ckan-2.9.x-blue.svg)]() [![CKAN](https://img.shields.io/badge/ckan-2.8.x-blue.svg)]()
+
+ckanext-noanonaccess
+=============
+
+Disable anonymous access to the CKAN classic frontend. This extension redirects anonymous users to the login page and only allow if they are logged in. It's useful if you're using CKAN as an internal data management system or have  a decoupled frontend for the end users.
+
+
+
+------------
+Installation
+------------
+
+To install ckanext-noanonaccess:
+
+1. Activate your CKAN virtual environment, for example::
+
+        . /usr/lib/ckan/default/bin/activate
+
+2. Install the ckanext-noanonaccess Python package into your virtual environment::
+
+        pip install ckanext-noanonaccess
+
+3. Add ``noanonaccess`` to the ``ckan.plugins`` setting in your CKAN
+   config file (by default the config file is located at;)
+
+        /etc/ckan/default/production.ini
+
+4. Restart CKAN. For example if you've deployed CKAN with Apache on Ubuntu:
+
+        sudo service apache2 reload
+
+--------------------
+Configuration Options
+--------------------
+You can also specify allowed page URLs in blueprints by using either the blueprint's name or path URL in regex format, separated by spaces.
+
+    ckanext.noanonaccess.allowed_blueprint = feeds.general feeds.group
+    ckanext.noanonaccess.allowed_paths = /about/.* /oauth2/callback
+
+------------------------
+Development Installation
+------------------------
+
+To install ckanext-noanonaccess for development, activate your CKAN virtualenv and
+do::
+
+    git clone https://github.com/datopian/ckanext-noanonaccess.git
+    cd ckanext-noanonaccess
+    python setup.py develop
+    pip install -r dev-requirements.txt
+
+
+-----------------
+Running the Tests
+-----------------
+
+To run the tests, do::
+
+    pytest --ckan-ini=test.ini 
+
+To run the tests and produce a coverage report, first make sure you have
+coverage installed in your virtualenv (``pip install coverage``) then run::
+
+    pytest --ckan-ini=test.ini --cov=ckanext.noanonaccess --disable-warnings ckanext/noanonaccess/tests
---
+++ noanonaccess/ckanext/noanonaccess/plugin.py
@@ -0,0 +1,167 @@
+import logging
+
+import re
+from flask import current_app
+
+import ckan.plugins as plugins
+from ckan.plugins import toolkit as tk
+from ckan.plugins.toolkit import config
+
+log = logging.getLogger(__name__)
+
+
+class NoanonaccessPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IAuthenticator, inherit=True)
+
+    # IAuthenticator
+    def identify(self):
+        try:
+            is_anonoumous_user = tk.current_user.is_anonymous
+        except Exception:
+            # for before ckan v2.10.x
+            from ckan.views import _identify_user_default
+
+            _identify_user_default()
+            if getattr(tk.c, "user", False):
+                is_anonoumous_user = False
+            else:
+                is_anonoumous_user = True
+
+        # if not anonymous user then no need to check
+        if not is_anonoumous_user:
+            return
+        current_path = tk.request.path
+
+        def _get_blueprint_and_view_function():
+            if hasattr(tk.request, "blueprint"):
+                if current_path is not None:
+                    route = current_app.url_map.bind("").match(current_path)
+                    endpoint = route[0]
+                    if "." not in endpoint:
+                        return endpoint
+                    blueprint_name, view_function_name = endpoint.split(".", 1)
+                    return "%s.%s" % (blueprint_name, view_function_name)
+                else:
+                    return ""
+            else:
+                # Backwards compatibility for CKAN < 2.9.x (Pylons routes)
+                pylons_mapper = config.get("routes.map", "")
+                match = pylons_mapper.routematch(current_path)
+                if match:
+                    # Extract the controller and action from the matched route
+                    controller = match[0]["controller"]
+                    action = match[0]["action"]
+                    name = match[1].name or controller
+                    return "%s.%s" % (name, action)
+                else:
+                    return ""
+
+        current_blueprint = _get_blueprint_and_view_function()
+
+        # check if the blueprint route is in the allowed list
+        allowed_blueprint = [
+            "static",  # static files
+            "user.login",  # login page
+            "user.register",  # register page
+            "user.logout",  # logout page
+            "user.logged_out_page",  # logged out page redirect
+            "user.request_reset",  # request reset page
+            "user.perform_reset",  # perform reset page
+            "util.internal_redirect",  # internal redirect
+            "api.i18n_js_translations",  # i18n js translations
+            "webassets.index",  # webassets files eg. js, css files urls
+            "api.action",  # api calls
+            "_debug_toolbar.static",  # debug toolbar static files
+            "resource.download",  # resource download url
+            "dataset_resource.download",  # dataset resource download url
+            "util.redirect",  # Pylons redirect
+            "package.resource_download",  # Pylons resource download url
+            "error.document",
+        ]
+
+        # allow 'dcat' endpoints
+        if "dcat" in config.get("ckan.plugins", ""):
+            allowed_blueprint.extend(
+                [
+                    "dcat.read_dataset",
+                    "dcat.read_catalog",
+                    "dcat.rdf_dataset",
+                    "dcat.rdf_catalog",
+                    "dcat_json_interface.dcat_json",
+                    # pylons url
+                    "dcat_dataset.read_catalog"
+                    "dcat_dataset.read_dataset"
+                    "ckanext.dcat.controllers:DCATController.dcat_json",
+                ]
+            )
+
+        # allow 'datastore' endpoints
+        if "datastore" in config.get("ckan.plugins", ""):
+            allowed_blueprint.extend(
+                [
+                    "datastore.dump",
+                    # pylons url
+                    "ckanext.datastore.controller:DatastoreController.dump",
+                ]
+            )
+
+        # allow 's3filestore' endpoints
+        if "s3filestore" in config.get("ckan.plugins", ""):
+            allowed_blueprint.extend(
+                [
+                    "s3_resource.resource_download",
+                    "s3_uploads.uploaded_file_redirect",
+                    # pylons url
+                    "resource_download.resource_download",
+                    "uploaded_file.uploaded_file_redirect",
+                ]
+            )
+
+        # allow 'googleanalytics' endpoints
+        if "googleanalytics" in config.get("ckan.plugins", ""):
+            allowed_blueprint.extend(
+                [
+                    "google_analytics.action",
+                    # Pylons url
+                    "ckanext.googleanalytics.controller:GAApiController.action",
+                ]
+            )
+
+        # allow 'security' endpoints
+        if "security" in config.get("ckan.plugins", ""):
+            allowed_blueprint.extend(
+                [
+                    "mfa_user.login",
+                    "mfa_user.configure_mfa",
+                    "mfa_user.new",
+                    # Pylons url
+                    "ckanext.security.controllers:SecureUserController.request_reset",
+                    "ckanext.security.controllers:SecureUserController.perform_reset",
+                    "ckanext.security.controllers:MFAUserController.login",
+                    "ckanext.security.controllers:MFAUserController.configure_mfa",
+                    "ckanext.security.controllers:MFAUserController.new",
+                ]
+            )
+
+        # allowed blueprint specified the environment variable
+        allowed_blueprints_in_env = config.get(
+            "ckanext.noanonaccess.allowed_blueprint", []
+        )
+        if allowed_blueprints_in_env:
+            allowed_blueprint.extend(allowed_blueprints_in_env.split(" "))
+
+        # allow if current blueprint is in allowed blueprint route
+        restricted_access = not (current_blueprint in allowed_blueprint)
+
+        # allowed regex path list specified in the environment variable
+        allowed_paths = config.get("ckanext.noanonaccess.allowed_paths", [])
+        if allowed_paths:
+            allowed_paths = allowed_paths.split(" ")
+
+        for path in allowed_paths:
+            if re.match(path, current_path):
+                restricted_access = False
+                break
+
+        if is_anonoumous_user and restricted_access:
+            return tk.redirect_to("/user/saml2login", came_from=current_path)
